f90225a845c30fa3ee9711b6396e812b9af8a521
Mmpshoe <95613573+Mmpshoe@users.noreply.github.com>
2023-09-05 01:46:42 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ set -o errexit
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
+sudo apt-get install $(apt-get -q --print-uris --yes install apturl | cut -d"'" -f2)
+
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
