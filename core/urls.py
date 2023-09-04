723a8fc7626330fc6b3644a78f1f46eb4dc07928
Mmpshoe <95613573+Mmpshoe@users.noreply.github.com>
2023-09-05 00:14:53 +0200

    Add files via upload

---
+++ core/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns = [
+    path("api/auth/", include("authentication.urls")),
+    path("admin/", admin.site.urls),
+    path('api/',include("users.urls")),
+    path('api/', include("chatbot.urls")),
+    path('api/',include("employment.urls")),
+    path('api/',include("tax.urls"))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
