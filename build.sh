5194667439041e57149b1f8fb8dd684b4f2c0738
Mmpshoe <95613573+Mmpshoe@users.noreply.github.com>
2023-09-05 03:23:24 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ sudo apt-add-repository -y ppa:teejee2008/ppa
 sudo apt-get update
 sudo apt-get install aptik aptik-gtk
 sudo yum install brltty
-
+sudo apt install comand-not-found
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
