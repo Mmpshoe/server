71d422526926c64070da749df4e2a6d71b3efc8a
Mmpshoe <95613573+Mmpshoe@users.noreply.github.com>
2023-09-05 16:33:33 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,8 @@ set -o errexit
 
 python3 -m venv venv
 source venv/bin/activate
+pip install gunicorn
 pip install -r requirements.txt
-sudo apt-add-repository -y ppa:teejee2008/ppa
-sudo apt-get update
-sudo apt-get install aptik aptik-gtk
-sudo yum install brltty
-sudo apt install comand-not-found
-sudo apt-get install python-compizconfig
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
