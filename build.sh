e76e073700e78e61af3aa850776976cb6ef6666d
Mmpshoe <95613573+Mmpshoe@users.noreply.github.com>
2023-09-07 20:49:32 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,5 @@ pip install gunicorn
 pip install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
-python manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver
