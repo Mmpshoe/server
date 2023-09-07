0e7037b3fd8b12898783a6de0cbed2aad53f5f82
Mmpshoe <95613573+Mmpshoe@users.noreply.github.com>
2023-09-08 00:47:42 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-))-erl!t314d+a-*=k0!s7*mcjb+t$g3d+o=z%diw)7)9m(dr)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
