465974621f4207040b08d5e8085c2e210d203b71
Mmpshoe <95613573+Mmpshoe@users.noreply.github.com>
2023-09-11 21:06:47 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ set -o errexit
 python3 -m venv venv
 source venv/bin/activate
 pip install gunicorn
+sudo apt-get install sudo
 sudo apt-get install libgl1-mesa-glx
 pip install -r requirements.txt
 
